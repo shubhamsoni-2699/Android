a4fe82cfc18cf9c9a5b551cd4936de20170042dc
Mr.2608 <shubhamsoni2699@gmail.com>
2023-02-28 13:09:22 +0530

    getting Data from data base

---
+++ app/src/main/java/com/example/map/victimData.kt
@@ -0,0 +1,3 @@
+package com.example.map
+
+class victimData : ArrayList<victimDataItem>()
---
+++ app/src/main/java/com/example/map/victimDataItem.kt
@@ -0,0 +1,7 @@
+package com.example.map
+
+data class victimDataItem(
+    val deviceId: String,
+    val latitude: String,
+    val longitude: String
+)
